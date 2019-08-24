9ad46c44fd8df351d820add796e6a65dcfe6c0c1
beaudrychase <beaudrychase@utexas.edu>
2019-08-24 16:19:20 -0500

    added comment_scrape.py

---
+++ comment_scrape.py
@@ -0,0 +1,15 @@
+import glob
+import json
+from comment_parser import comment_parser
+
+count = 0
+for filename in glob.iglob("/home/pi/" + '**/*.java', recursive=True):
+     print(filename)
+     json_obj = json.dump("{'text':[]}")
+     all_comments = comment_parser.extract_comments(filename, "text/x-java-source")
+     for comment in all_comments:
+     	json_obj[text].add(comment.text())
+ 	with open("./results/" + str(count), 'w') as file:
+ 		json.dump(json_obj, file)
+	count += 1
+
