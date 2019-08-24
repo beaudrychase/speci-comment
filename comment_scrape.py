fc5bf4f66865f931433cde5d73ed1db2ce4e9744
beaudrychase <beaudrychase@utexas.edu>
2019-08-24 22:31:23 +0100

    small edit

--- comment_scrape.py
+++ comment_scrape.py
@@ -9,7 +9,7 @@ for filename in glob.iglob("/home/pi/java_projects" + '**/*.java', recursive=Tru
      all_comments = comment_parser.extract_comments(filename, "text/x-java-source")
      for comment in all_comments:
      	json_obj[text].add(comment.text())
- 	with open("./results/" + str(count), 'w') as file:
- 		json.dump(json_obj, file)
+        with open("./results/" + str(count), 'w') as file:
+            json.dump(json_obj, file)
 	count += 1
 
